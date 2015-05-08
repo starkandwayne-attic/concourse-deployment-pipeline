be917e2dfa494d73624f40e822c24c6537eabddd
Dr Nic Williams <drnicwilliams@gmail.com>
2015-05-08 09:30:53 -0700

    remove tests for logstash-docker stuff

--- production/bin/create_stub_make_manifest_and_save.sh
+++ production/bin/create_stub_make_manifest_and_save.sh
@@ -6,36 +6,5 @@ manifest=${manifest:-"manifests/manifest.yml"}
 set -e
 
 cd $DIR/..
-mkdir -p manifests
-stage=$(basename $DIR)
-
-if [[ ! -f releases/concourse/version ]]; then
-  echo "missing resource input ${stage}/releases/concourse/version"
-  exit 1
-fi
-LOGSTASH_DOCKER_VERSION=$(cat releases/concourse/version)
-
-if [[ ! -f releases/docker/version ]]; then
-  echo "missing resource input ${stage}/releases/docker/version"
-  exit 1
-fi
-DOCKER_VERSION=$(cat releases/docker/version)
-
-if [[ ! -f stemcell/version ]]; then
-  echo "missing resource input ${stage}/stemcell/version"
-  exit 1
-fi
-STEMCELL_VERSION=$(cat stemcell/version)
-
-cat >manifests/pipeline-inputs.yml <<EOF
----
-meta:
-  release_versions:
-    logstash_docker: "$LOGSTASH_DOCKER_VERSION"
-    docker: "$DOCKER_VERSION"
-  stemcell:
-    version: "$STEMCELL_VERSION"
-EOF
-
 
 ./bin/make_manifest.sh $@ > $manifest
